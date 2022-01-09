2aebad2c9b0f693f9112c5065db00d3868a1f680
prakhar146 <prakhar.14697@gmail.com>
2022-01-09 11:21:51 +0530

    Time: 15 ms (29.39%), Space: 42.8 MB (13.49%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.java
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.java
@@ -4,51 +4,26 @@ class Solution {
             return 0;
         }
 
-        int ptr1 = 0;
+      int ptr1 = 0;
         int ptr2 = 0;
-        int currentLength = 0;
         int maxLength = 0;
-        boolean[] exists = new boolean[26];
-        // exists[s.charAt(0) - 97] = true;
-        String maxLengthSubstring = "";
+        HashSet<Character> c = new HashSet<>();
 
-         Map<Character, Boolean> existMap = new HashMap<>();
-        existMap
-                .put(s.charAt(ptr1), true);
-
-        while (ptr1 <= ptr2 && ptr2 < s.length()) {
-            if (ptr1 == ptr2) {
+        while (ptr2 < s.length()) {
+            if (!c.contains(s.charAt(ptr2))) {
+                c.add(s.charAt(ptr2));
                 ptr2++;
-                continue;
-            }
-            if (existMap.containsKey(s.charAt(ptr2))) {
-                currentLength = ptr2 - ptr1;
-                if (currentLength > maxLength) {
-                    maxLength = currentLength;
-                    maxLengthSubstring = s.substring(ptr1, ptr2);
-                }
-//                exists = new boolean[26];
-                existMap.clear();
-                ptr1++;
-//                exists[s.charAt(ptr1) - 97] = true;
-                existMap
-                        .put(s.charAt(ptr1), true);
-                ptr2 = ptr1;
             } else {
-//                exists[s.charAt(ptr2) - 97] = true;
-                existMap
-                        .put(s.charAt(ptr2), true);
-                ptr2++;
+                int currentLength = ptr2 - ptr1;
+                maxLength = Math.max(currentLength, maxLength);
+                c.remove(s.charAt(ptr1));
+                ptr1++;
             }
         }
 
-        currentLength = ptr2 - ptr1;
-        if (currentLength > maxLength) {
-            maxLength = currentLength;
-            maxLengthSubstring = s.substring(ptr1, ptr2);
-        }
+        int currentLength = ptr2 - ptr1;
+        maxLength = Math.max(currentLength, maxLength);
 
-
-        return maxLengthSubstring.length();
+        return maxLength;
     }
 }
