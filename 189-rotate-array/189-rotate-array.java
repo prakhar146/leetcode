3b60f043b0bb56c09acd4b19e47db5863104e406
prakhar146 <prakhar.14697@gmail.com>
2022-01-31 09:40:57 +0530

    Time: 0 ms (100.00%), Space: 64.6 MB (6.09%) - LeetHub

--- 189-rotate-array/189-rotate-array.java
+++ 189-rotate-array/189-rotate-array.java
@@ -1,10 +1,19 @@
 class Solution {
     public void rotate(int[] nums, int k) {
         k = k % nums.length;
-        int[] arr2 = Arrays.copyOf(nums, nums.length);
-        for(int i = 0; i < nums.length; i++) {
-            nums[(i + k) % nums.length] = arr2[i];
-        }
+        reverse(nums, 0, nums.length - k - 1);
+        reverse(nums, nums.length - k, nums.length - 1);
+        reverse(nums, 0, nums.length - 1);
         return;
+    }
+    
+    void reverse(int[] arr, int l, int r) {
+        while(l < r) {
+            int tmp = arr[l];
+            arr[l] = arr[r];
+            arr[r] = tmp;
+            l++;
+            r--;
+        }
     }
 }
