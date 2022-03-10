788d46d3872a3a55e5bd0b00c7bb00bd0626a192
prakhar146 <prakhar.14697@gmail.com>
2022-03-11 02:17:45 +0530

    Time: 39 ms (56.88%), Space: 52.3 MB (92.15%) - LeetHub

--- 164-maximum-gap/164-maximum-gap.java
+++ 164-maximum-gap/164-maximum-gap.java
@@ -3,90 +3,77 @@ class Solution {
         if(nums.length < 2) {
             return 0;
         }
-        int numOfIntervals = nums.length - 1;
-        int min = Integer.MAX_VALUE;
-        int max = Integer.MIN_VALUE;
-        for(int i: nums) {
-            min = Math.min(min, i);
-            max = Math.max(max, i);
-        }
-        
-        Integer[] minBuckets = new Integer[numOfIntervals];
-        Integer[] maxBuckets = new Integer[numOfIntervals];
-        
-        fillBuckets(minBuckets, maxBuckets, numOfIntervals, min, max, nums);
-        
-        // System.out.println("min bucket -> " + Arrays.toString(minBuckets));
-        // System.out.println("max bucket -> " + Arrays.toString(maxBuckets));
-        
-        return compareBuckets(minBuckets, maxBuckets);
-    }
-    
-    void fillBuckets(Integer[] minBuckets, Integer[] maxBuckets, int numOfIntervals, int min, int max, int[] nums) {
-        // System.out.println("min -> " + min + " max -> " + max);
-        int avgInterval = (int)Math.ceil((double)(max + 1 - min) / (double)numOfIntervals);
-        
-        // System.out.println("avg interval size -> " + avgInterval);
+        int min = nums[0];
+        int max = nums[1];
         
         for(int i: nums) {
-            int bucketIdx = (i - min) / avgInterval;
-            if(minBuckets[bucketIdx] == null) {
-                minBuckets[bucketIdx] = i;
-            }
-            if(maxBuckets[bucketIdx] == null) {
-                maxBuckets[bucketIdx] = i;
-            }
-            minBuckets[bucketIdx] = Math.min(minBuckets[bucketIdx], i);
-            maxBuckets[bucketIdx] = Math.max(maxBuckets[bucketIdx], i);
-        }
-    }
-    
-    int compareBuckets(Integer[] minBuckets, Integer[] maxBuckets) {
-    
-        int max = 0;
-        
-        for(int i = 0; i < minBuckets.length; i++) {
-            if(minBuckets[i] != null) {
-                max = maxBuckets[i] - minBuckets[i];
-                break;
-            }
-        } 
-        
-        int ptr1 = 0, ptr2 = 0;
-        
-        while(ptr2 < minBuckets.length) {
-            if(ptr2 <= ptr1 || (minBuckets[ptr2] == null)) {
-                ptr2++;
-            } else {
-                max = Math.max(max, minBuckets[ptr2] - maxBuckets[ptr1]);
-                ptr1 = ptr2;
-                ptr2++;
-            }
+            min = Math.min(i, min);
+            max = Math.max(i, max);
         }
         
-        // for(int i = 1; i < minBuckets.length; i++) {
-        //     if(minBuckets[i] != null && maxBuckets[i - 1] != null) {
-        //          max = Math.max(max, minBuckets[i] - maxBuckets[i - 1]);
-        //     }
+        int minPossibleAns = (int) Math.ceil(((double) (max - min)) / nums.length);
+        
+        if(minPossibleAns == 0) {
+            return 0;
+        }
+        
+        int maxGap = minPossibleAns;
+        
+        LinkedList<Integer>[] buckets = new LinkedList[nums.length];
+        // for(int i = 0; i < nums.length; i++) {
+        //     buckets[i] = new ArrayList<Integer>();
         // }
-        // max = Math.max()
         
-        return max;
+        for(int i = 0; i < nums.length; i++) {
+            buckets[i] = new LinkedList<Integer>();
+        }
+        
+        // System.out.println("min poss ans -> " + minPossibleAns);
+        
+        for(int i: nums) {
+            int diff = (i - min);
+            int bucketIdx =  (diff != 0 && diff % minPossibleAns == 0) ? ((diff / minPossibleAns) - 1): (diff / minPossibleAns);
+            // System.out.print("num -> " + i + " idx -> " + bucketIdx);
+            LinkedList<Integer> bucket = buckets[bucketIdx];
+            if(bucket.size() > 0) {
+                int bucketMin = bucket.getFirst();
+                int bucketMax = bucket.getLast();
+                if(i < bucketMin) {
+                    // bucket.set(0, i);
+                    bucket.addFirst(i);
+                }
+                if (i > bucketMax) {
+                    // bucket.set(nums.length - 1, i);
+                    bucket.addLast(i);
+                }
+            } else {
+                bucket.addFirst(i);                
+            }
+            // System.out.println(" bucket -> " + buckets[bucketIdx]);
+        }
+        
+        int idx = 0;
+        int currentValue = -1;
+        int lastValue = -1;
+        while(idx < nums.length) {
+            if(buckets[idx].size() > 0) {
+                // currentValue = buckets[idx].get(0);
+                currentValue = buckets[idx].getFirst();
+                if(lastValue > -1) {
+                    maxGap = Math.max(maxGap, currentValue - lastValue);
+                }
+                // lastValue = buckets[idx].get(buckets[idx].size() - 1);
+                lastValue = buckets[idx].getLast();
+            }
+            idx++;
+        }
+        
+        return maxGap;
     }
 }
 
-/* 
+/*
 
-3, 6, 9, 1
- intervals = 3
- min = 1
- max = 9
-
-avgInterval = 8 / 3 = ceil(2.34) = 3
-
-2 / 3 = 0
-5 / 3 = 1.6 = 
-
-
+1,3,6,9 
 
 */
