059d0c5da30832bf3b58a717f6b06e81f8117910
prakhar146 <prakhar.14697@gmail.com>
2022-06-28 11:26:03 +0530

    Time: 3 ms (38.69%), Space: 44.5 MB (72.23%) - LeetHub

--- 152-maximum-product-subarray/152-maximum-product-subarray.java
+++ 152-maximum-product-subarray/152-maximum-product-subarray.java
@@ -1,23 +1,43 @@
 class Solution {
     public int maxProduct(int[] nums) {
-        int ans = Integer.MIN_VALUE;
-        for(int num: nums) {
-            ans = Math.max(ans, num);
-        }
+        int maxValue = nums[0];
+        int minValue = nums[0];
+        
         int currentMax = 1, currentMin = 1;
-        for(int num: nums) {
-            if(num == 0) {
-                currentMax = 1; 
-                currentMin = 1;
-                continue;
-            }
-            int tmp = Math.max(num, Math.max(currentMax * num, currentMin * num));
-            currentMin = Math.min(num, Math.min(currentMax * num, currentMin * num));
-            currentMax = tmp;
-            // System.out.println("num: " + num + " cMax " + currentMax + " cMin " + currentMin);
-            ans = Math.max(ans, currentMax);
+        
+        for(int i = 0; i < nums.length; i++) {
+            int v1 = currentMax * nums[i];
+            int v2 = currentMin * nums[i];
+            
+            int tmpMax = Math.max(v1, v2);
+            int tmpMin = Math.min(v1, v2);
+            
+            currentMax = Math.max(nums[i], tmpMax);
+            currentMin = Math.min(nums[i], tmpMin);
+            
+            maxValue = Math.max(currentMax, maxValue);
+            minValue = Math.min(currentMin, minValue);
+            
+            if(currentMax == 0) currentMax = 1;
+            if(currentMin == 0) currentMin = 1;
         }
         
-        return ans;
+        return maxValue;
     }
 }
+
+/*
+[]
+
+nums[i] CurrMax CurrMin MaxV MinV 
+            1       1     1    1
+----------------------------
+     2      2       2     2     2
+    -5     -5      -10    2     -10
+    -2     10       -2    10    -10
+    -4      8      -40
+    
+    -2      -2      -2     -2   -2
+    0       0       0       0   -2
+    -1      -1      -1      0   -2    
+*/
