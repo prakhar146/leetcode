a7a7e56fdf3a2e10d0a3312258b7e411a4b78936
prakhar146 <prakhar.14697@gmail.com>
2022-07-03 01:23:43 +0530

    Time: 1 ms (72.61%), Space: 43.5 MB (6.44%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.java
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.java
@@ -1,68 +1,59 @@
 class Solution {
-    public int search(int[] nums, int target) {
-        int startPt = getStartPt(nums);
-        // System.out.println("Starting Pt " + startPt);
-        return binarySearch(startPt, nums, target);
-    }
-    
-    int binarySearch(int startIdx, int[] nums, int target) {
-        int l = startIdx;
-        int e = l + nums.length - 1;
+    public int search(int[] nums, int target) {        
+        int pivot = getPivot(nums);
+        // System.out.println("Pivot " + pivot);
+        int left = pivot;
+        int right = pivot + nums.length;
         
-        while(l <= e) {
-            int mid = (l + e) / 2;
+        while(left < right) {
+            int mid = (left + right) / 2;
             int midNum = nums[mid % nums.length];
-            int lNum = nums[l % nums.length];
-            // System.out.println("left " + l + " end " + e + " mid " + mid + " midnum " + midNum);
-            // int rNum = nums[e % nums.length];
-            // System.out.println("l " + (l % nums.length) + " end " + (e % nums.length) + " mid " + midNum);
+            // System.out.println("left " + left + " right " + right + " mid " + midNum);
             if(midNum == target) {
                 return (mid % nums.length);
             }
             if(midNum < target) {
-                l = mid + 1;
+                left = mid + 1;
             } else {
-                e = mid - 1;
+                right = mid;
             }
         }
         
         return -1;
     }
     
-    int getStartPt(int[] nums) {
-        int s = 0;
-        int e = nums.length;
+    int getPivot(int[] nums) {
+        int n = nums.length;
+        int left = 0;
+        int right = n - 1;
         
-        while(s < e) {
-            int mid = (s + e) / 2;
-            if(nums[mid] > nums[s]) {
-                s = mid;
-            } else {
-                e = mid;
+        if(nums[n - 1] >= nums[0]) {
+            return 0;
+        }
+        int mid = 0;
+        while(left < right) {
+            mid = (left + right) / 2;
+            int midNum = nums[mid];
+            
+            if(midNum > nums[left] && midNum > nums[right]) {
+                left = mid + 1;
+                continue;
+            }
+            if(midNum < nums[left] && midNum < nums[right]) {
+                right = mid;
+                continue;
+            }
+            if(midNum >= nums[left] && midNum <= nums[right]) {
+                right = left;
+                continue;
+            }
+            if(midNum <= nums[left] && midNum >= nums[right]) {
+                left = right;
+                continue;
             }
         }
+        mid = (left + right) / 2;
         
-        return s + 1;
+        return mid;
     }
 }
-/*
-[4, 5, 6, 7, 0, 1, 2] --> 7
- 0, 1, 2, 3, 4, 5, 6 ,7
-getPivot()
-s = 0
-e = 7
-while(s < e) {
-    mid = (0 + 7) / 2 = 3
-    if(nums[3] > nums[0]) --> if(7 > 4) --> true
-        s = 3
-    -----
-    mid = (4 + 7) / 2 = 5
-    if(nums[5] > nums[4]) --> if(1 > 0) --> true
-        s = 6
-    -----
-    mid = (6 + 7) / 2 = 6
-    if(nums[6] > nums[6]) --> if(1 > 0) --> true
-        s = 6
-}
-
-*/
