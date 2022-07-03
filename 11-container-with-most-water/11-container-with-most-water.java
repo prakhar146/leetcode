19c8163ea318911b9a3d0a224dc747a206d13443
prakhar146 <prakhar.14697@gmail.com>
2022-07-03 19:07:17 +0530

    Time: 7 ms (17.92%), Space: 82.1 MB (5.12%) - LeetHub

--- 11-container-with-most-water/11-container-with-most-water.java
+++ 11-container-with-most-water/11-container-with-most-water.java
@@ -1,43 +1,59 @@
 class Solution {
     public int maxArea(int[] height) {
-        return solution2(height);
-        // int l = 0, r = 0;
-        // int maxAns = -1, currentAns = -1;
-        // while(r < height.length) {
-        //     if(l == r) {
-        //         r++;
-        //     } else {
-        //         if(height[l] < height[r]) {
-        //             currentAns = (r - l) * (Math.min(height[r], height[l]));
-        //             maxAns = Math.max(currentAns, maxAns);
-        //             l = r;
-        //         } else {
-        //             r++;
-        //         }
-        //     }
-        // }
-        // if(height[l] >= height[r - 1]) {
-        //     currentAns = ((r - 1) - l) * (Math.min(height[l], height[r - 1]));
-        //     maxAns = Math.max(currentAns, maxAns);
-        // }
-        // return maxAns;
-    }
-    
-    int solution2(int height[]) {
-        int l = 0;
-        int r = height.length - 1;
-        int currentAns = -1, maxAns = -1;
-        
-        while(l < r) {
-            currentAns = (r - l) * Math.min(height[l], height[r]);
-            maxAns = Math.max(currentAns, maxAns);
-            if(height[l] > height[r]) {
-                r--;
+        int n = height.length;
+        int left = 0;
+        int right = n - 1;
+        int maxWater = 0;
+        while(left < right) {
+            int w = (right - left);
+            int h = Math.min(height[right], height[left]);
+            int currentArea = h * w;
+            maxWater = Math.max(currentArea, maxWater);
+            if(height[right] > height[left]) {
+                left++;
+            } else if (height[left] > height[right]) {
+                right--;
             } else {
-                l++;
+                left++;
+                right--;
             }
         }
         
-        return maxAns;
+        return maxWater;
+    }
+    
+    int codeBlock(int[] height) {
+        int n = height.length;
+        int[] leftMax = new int[n];
+        int[] rightMax = new int[n];
+        leftMax[0] = height[0];
+        rightMax[n - 1] = height[n - 1];
+        for(int i = 1; i < n; i++) {
+            leftMax[i] = Math.max(leftMax[i - 1], height[i]);
+        }
+        for(int i = (n - 2); i > -1; i--) {
+            rightMax[i] = Math.max(rightMax[i + 1], height[i]);
+        }
+        System.out.println(Arrays.toString(leftMax));
+        System.out.println(Arrays.toString(rightMax));
+        
+        int maxWater = 0;
+        for(int i = 0; i < n; i++) {
+            maxWater += Math.min(leftMax[i], rightMax[i]);
+        }
+        return maxWater;
     }
 }
+/*
+
+1 - (1, 1)
+8
+    left (0,8)
+        4 -> -1
+        2 -> -1
+        1 -> 1
+    right (0,8)
+        4 -> 
+
+
+*/
