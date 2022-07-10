a4931e939604932c60237c5de9d4d647ee400967
prakhar146 <prakhar.14697@gmail.com>
2022-07-10 16:57:01 +0530

    Time: 75 ms (14.28%), Space: 46.6 MB (24.06%) - LeetHub

--- 322-coin-change/322-coin-change.java
+++ 322-coin-change/322-coin-change.java
@@ -1,29 +1,33 @@
 class Solution {
     public int coinChange(int[] coins, int amount) {
-        int[] amounts = new int[amount + 1];
-        for(int i = 0 ; i < amounts.length; i++) {
-            amounts[i] = amount + 1;
+        // Arrays.sort(coins, Collections.reverseOrder());
+        return getCoins(coins, amount, new Integer[amount + 1]);
+    }
+    
+    int getCoins(int[] coins, int amount, Integer[] dp) {
+        if(amount < 0) {
+            return -1;
         }
-        amounts[0] = 0;
-        
-//         int minCoin = Integer.MAX_VALUE;
-//         for(int coin: coins) {
-//             if(coin < minCoin) {
-//                 minCoin = coin;
-//             }
-//         }
-        
-//         amounts[minCoin] = 1;
-        
-        for(int i = 1; i < amounts.length; i++) {
-            for(int coin: coins) {
-                    if(i - coin >= 0 && amounts[i - coin] < amount) {
-                        amounts[i] = Math.min(amounts[i], amounts[i - coin] + 1);
-                    }
+        if(amount == 0) {
+            return 0;
+        }
+        if(dp[amount] != null) {
+            return dp[amount];
+        }
+        int N = coins.length;
+        int ans = -1;
+        for(int i = 0; i < N; i++) {
+            int tmpAmt = amount - coins[i];
+            int op = getCoins(coins, tmpAmt, dp);
+            if(op > -1) {
+                if(ans > -1) {
+                    ans = Math.min(ans, 1 + op);   
+                } else {
+                    ans = 1 + op;
                 }
+            }
         }
-        // System.out.println("Array -> " + Arrays.toString(amounts));
-        
-       return amounts[amount] > amount ? -1 : amounts[amount];
+        dp[amount] = ans;
+        return ans;
     }
 }
