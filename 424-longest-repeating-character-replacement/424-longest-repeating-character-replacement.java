56194731678d41715a45d5ed69e284cac9f8213e
prakhar146 <prakhar.14697@gmail.com>
2022-07-20 01:01:42 +0530

    Time: 9 ms (69.68%), Space: 42.9 MB (74.24%) - LeetHub

--- 424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.java
+++ 424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.java
@@ -1,24 +1,40 @@
 class Solution {
     public int characterReplacement(String s, int k) {
+        int[] freq = new int[26];
+        int l = 0, r = 0;
         int N = s.length();
-        int[] chars = new int[26];
-        int l = 0;
         int maxAns = 0;
-        int maxF = 0;
-        for(int r = 0; r < N; r++) {
-            chars[s.charAt(r) - 'A']++;
-            maxF = Math.max(maxF, chars[s.charAt(r) - 'A']);
-            int size = r + 1 - l;
-            int reqK = size - maxF;
+        while(r < N) {
+            char c = s.charAt(r);
+            freq[c - 'A']++;
+            
+            int totalChars = r + 1 - l;
+            int maxCharCnt = getMax(freq);
+            int reqK = totalChars - maxCharCnt;
             while(reqK > k) {
-                chars[s.charAt(l) - 'A']--;
+                // System.out.print(Arrays.toString(freq));
+                char leftChar = s.charAt(l);
+                freq[leftChar - 'A']--;
                 l++;
-                size = r + 1 - l;
-                reqK = size - maxF;
+                totalChars = (r + 1) - l;
+                maxCharCnt = getMax(freq);
+                reqK = totalChars - maxCharCnt;
+                // System.out.println("r -> " + r + "; l -> " + l + " reqK " + reqK + " totalChars -> " + totalChars + " maxCharCnt -> " + maxCharCnt);
             }
-            maxAns = Math.max(maxAns, size);
+            
+            maxAns = Math.max(maxAns, r + 1 - l);
+            r++;
         }
         
         return maxAns;
+    }
+    
+    int getMax(int[] freq) {
+        int max = 0;
+        for(int i: freq) {
+            max = Math.max(i, max);
+        }
+        
+        return max;
     }
 }
