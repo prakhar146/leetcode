6b05313810238152795d387e9d94f4b4f310268f
prakhar146 <prakhar.14697@gmail.com>
2022-07-30 18:35:10 +0530

    Time: 2 ms (27.14%), Space: 43.8 MB (39.62%) - LeetHub

--- 31-next-permutation/31-next-permutation.java
+++ 31-next-permutation/31-next-permutation.java
@@ -1,64 +1,38 @@
 class Solution {
-    public void nextPermutation(int[] nums) { 
-        int ptr = getPtOfChange(nums);
-        if(ptr > -1) {
-            Arrays.sort(nums, ptr, nums.length);
+    public void nextPermutation(int[] nums) {
+        int N = nums.length;
+        int left = -1;
+        int right = -1;
+        
+        // float up
+        // int maxNum = nums[N - 1];
+        for(int i = (N - 1); i > -1; i--) {
+            int min = 101;
+            for(int j = i; j < N; j++) {
+                // System.out.println("i -> " + i + " j -> " + j + " nums[" + i + "] -> " + nums[i] + " nums[" + j + "] -> " + nums[j]);
+                if(nums[j] > nums[i] && nums[j] < min) {
+                    left = i;
+                    right = j;
+                    min = nums[j];
+                    // break;
+                }
+            }
+            if(left > -1 && right > -1) {
+                break;
+            }
+        }
+        // System.out.println("left -> " + left + " right -> " + right);
+        if(left > -1 && right > -1) {
+            swap(nums, left, right);
+            Arrays.sort(nums, left + 1, N);   
         } else {
             Arrays.sort(nums);
         }
     }
     
-    int getPtOfChange(int[] nums) {
-        
-        int ptr = -1;
-        int currentAns = Integer.MAX_VALUE;
-        int l = -1;
-        int r = -1;
-        int n = nums.length - 1;
-        
-        for(int i = 0; i < n; i++) {
-            for(int j = (i + 1); j <= n; j++) {
-                if(nums[j] > nums[i]) {
-                    if(l != i) {
-                        currentAns = Integer.MAX_VALUE;
-                    }
-                    // System.out.println("nums[" + j + "] -> " + nums[j] + " nums[" + i + "] -> " + nums[i]);
-                    if(nums[j] < currentAns) {
-                        l = i;
-                        r = j;
-                        currentAns = nums[j];
-                        // System.out.println("nums[" + j + "] -> " + nums[j] + " nums[" + i + "] -> " + nums[i] + " l -> " + l + " r ->" + r);
-                    }
-                }
-            }
-        }
-        if(l > -1) {
-            swap(nums, l, r);   
-            ptr = l + 1;
-        }
-        return ptr;
-    }
-    
-    void swap(int[] nums, int idx1, int idx2) {
-        int tmp = nums[idx1];
-        nums[idx1] = nums[idx2];
-        nums[idx2] = tmp;
+    void swap(int[] nums, int left, int right) {
+        int tmp = nums[left];
+        nums[left] = nums[right];
+        nums[right] = tmp;
     }
 }
-
-/*
-Expected
-1,2,3
-1,3,2
-2,1,3
-2,3,1
-3,2,1
-
-1,2,3
-1,
-
-2,4,5,1,2,3,2,1
-2,4,5,2,2,3,1,1        
-
-
-*/
