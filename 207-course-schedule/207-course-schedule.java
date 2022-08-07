58f024bbe09602e59ea37a38c577a8e94dc0e44b
prakhar146 <prakhar.14697@gmail.com>
2022-08-08 02:17:05 +0530

    Time: 16 ms (25.36%), Space: 50.4 MB (14.83%) - LeetHub

--- 207-course-schedule/207-course-schedule.java
+++ 207-course-schedule/207-course-schedule.java
@@ -2,22 +2,14 @@ class Solution {
     public boolean canFinish(int numCourses, int[][] prerequisites) {
         Map<Integer, List<Integer>> graph = new HashMap<>();
         loadGraph(graph, prerequisites);
-        // printMap(graph);
+        Map<Integer, Boolean> visited = new HashMap<>();
+        Map<Integer, Boolean> dfsStack = new HashMap<>();
         boolean hasCycle = false;
-        // for(int i: graph.keySet()) {
-        //     hasCycle = hasCycle || hasCycle(graph, i);
-        // }
-        Set<Integer> visited = new HashSet<>();
-        Set<Integer> dfsVisited = new HashSet<>();
-        for(int i: graph.keySet()) {
-            if(!visited.contains(i)) {
-                boolean ans = hasCycleDFS(graph, i, visited, dfsVisited);
-                // System.out.println("Node " + i + " ans " + ans);
-                hasCycle = hasCycle || ans;
+        for(int course: graph.keySet()) {
+            if(!visited.containsKey(course)) {
+                hasCycle = hasCycle || hasCycle(graph, visited, dfsStack, course);
             }
-            
         }
-        
         return !hasCycle;
     }
     
@@ -34,55 +26,23 @@ class Solution {
         }
     }
     
-    boolean hasCycleDFS(Map<Integer, List<Integer>> graph, int src, Set<Integer> visited, Set<Integer> dfsVisited) {
-        if(visited.contains(src)) {
-            return false;
-        }
-        // System.out.println("Checking for " + src);
+    boolean hasCycle(Map<Integer, List<Integer>> graph, Map<Integer, Boolean> visited, Map<Integer, Boolean> dfsStack, int src) {
+        dfsStack.put(src, true);
+        visited.put(src, true);
+        List<Integer> nodesToVisit = graph.get(src);
         boolean hasCycle = false;
-        dfsVisited.add(src);
-        visited.add(src);
-        if(!graph.containsKey(src)) {
-            dfsVisited.remove(src);
-            return false;
-        }
-        for(int nextNode: graph.get(src)) {
-            if(dfsVisited.contains(nextNode)) {
-                return true;
-            }
-            if(hasCycleDFS(graph, nextNode, visited, dfsVisited)) {
-                return true;
-            }
-        }
-        dfsVisited.remove(src);
-        return hasCycle;
-    }
-    
-    boolean hasCycle(Map<Integer, List<Integer>> graph, int src) {
-        Queue<Integer> queue = new LinkedList<>();
-        queue.offer(src);
-        Set<Integer> hasVisited = new HashSet<>();
-        while(!queue.isEmpty()) {
-            int node = queue.poll();
-            if(node == 5) {
-                // System.out.print("currentnode -> " + node);
-            }
-            if(hasVisited.contains(node)) {
-                // System.out.println("currentnode -> " + node + " visited -> true");
-                return true;
-            }
-            hasVisited.add(node);
-            List<Integer> nodesToVisit = graph.get(node);
-            if(nodesToVisit != null && nodesToVisit.size() > 0) {
-                for(int nextNode: nodesToVisit) {
-                     // System.out.println(" nextNode -> " + nextNode);
-                    queue.offer(nextNode);
+        if(nodesToVisit != null) {
+            for(int node: graph.get(src)) {
+                if(dfsStack.containsKey(node)) {
+                    return true;
                 }
-            }
+                if(!visited.containsKey(node)) {
+                    hasCycle = hasCycle || hasCycle(graph, visited, dfsStack, node);
+                }
+            }   
         }
-        
-        return false;
-        
+        dfsStack.remove(src);
+        return hasCycle;
     }
     
     void printMap(Map<Integer, List<Integer>> graph) {
